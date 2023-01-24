8480184f9a1552c910eb73076bbd29bfeeaa1f7f
JacopSven <jacob.svensson2@elev.ga.ntig.se>
2023-01-24 10:39:40 +0100

    2Values Plus EZ

---
+++ 2Values.js
@@ -0,0 +1,5 @@
+function plus(val1, val2) { // Skapar en funktion som heter plus som säger till att när man får in två värden så plusar den ihop dem.
+    return val1 + val2;
+}
+
+console.log(plus(1,2)); // Denna skriver ut talet i konsolen.

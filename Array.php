14f87df06b56f5a25b7fd4c0772fafc595174cf8
JacopSven <jacob.svensson2@elev.ga.ntig.se>
2023-01-24 13:05:10 +0100

    .

--- Array.php
+++ Array.php
@@ -4,14 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Asrray</title>
+    <title>Array</title>
 </head>
 <body>
 <script>
-    let array=([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    let array = [111, 222, 382, 400, 520, 610, 7731, 8333, 9312, 1000]; // Sätter en variabel array med 10st nummer
+    let sum = 0; // Sätter en variablel sum till 0 i början
 
-    function
+    array.forEach(number => { // En script som loopar för varje nummer i arrayn och lägger till det nummret till variablem sum.
+        sum += number;
+    });
     
+    console.log(sum/10); // Här printar vi ut varablen sum och delar den på antalet nummer för att få ut medelvärdet
 </script>
 </body>
 </html>

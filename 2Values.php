9f50c4cd0319ed5f4a866624a9f4c8a1a6e4e5d7
JacopSven <jacob.svensson2@elev.ga.ntig.se>
2023-01-24 12:45:51 +0100

    hej

--- 2Values.php
+++ 2Values.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tal+5</title>
+    <title>2Values</title>
 </head>
 <body>
 <script>
-    
+
     function plus(val1, val2) { // Skapar en funktion som heter plus som säger till att när man får in två värden så plusar den ihop dem.
         return val1 + val2;
     }
---
+++ Array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Asrray</title>
+</head>
+<body>
+<script>
+    let array=([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+    function
+    
+</script>
+</body>
+</html>

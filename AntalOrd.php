c0642ba93672650169f313c79b5eb6dc0dd3c155
JacopSven <jacob.svensson2@elev.ga.ntig.se>
2023-01-24 13:15:13 +0100

    ,

---
+++ AntalOrd.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AntalOrd</title>
+</head>
+<body>
+<script>
+    let string = ("Yo! Fabian Is Dugly");
+    let words = 0;
+
+    array = string.split(" ");
+
+    array.forEach(Number => {
+        words += 1;
+    });
+
+    console.log(words);
+
+
+</script>
+</body>
+</html>l
